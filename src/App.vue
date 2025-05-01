38c06b743d47eb66972c89b1408735e5b55a57f2
Amoseven <omsk.web@gmail.com>
2025-05-01 17:14:29 +0600

    add computed property

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
+<template>
+  <h1>{{ currentFullDate }}</h1>
+</template>
+
 <script setup>
 import {computed} from "vue";
 
 function formatDate(date) {
-
   let dd = date.getDate().toString().padStart(2, '0');
   let mm = (date.getMonth() + 1).toString().padStart(2, '0')
   let yy = date.getFullYear()
@@ -18,10 +21,5 @@ const currentFullDate = computed(() => formatDate(new Date()))
 
 </script>
 
-<template>
-  <h1>{{ currentFullDate }}</h1>
-</template>
-
 <style scoped>
-
 </style>
