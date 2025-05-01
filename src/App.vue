448181323cbc60f19fb4ec51c0cf037e7b4ff595
Amoseven <omsk.web@gmail.com>
2025-05-01 16:02:58 +0600

    first task completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
+function formatDate(date) {
+
+  let dd = date.getDate();
+  if (dd < 10) dd = '0' + dd;
+
+  let mm = date.getMonth() + 1;
+  if (mm < 10) mm = '0' + mm;
+
+  let yy = date.getFullYear()
+
+  return dd + '.' + mm + '.' + yy;
+}
+
+const currentDate = formatDate(new Date());
 </script>
 
 <template>
-
+  <h1>{{ currentDate}}</h1>
 </template>
 
 <style scoped>
