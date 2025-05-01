ab0a92ef744ece79f512be35018af2baa5cdef9e
Amoseven <omsk.web@gmail.com>
2025-05-01 16:38:17 +0600

    add computed property

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup>
+import {computed} from "vue";
+
 function formatDate(date) {
 
-  let dd = date.getDate();
-  if (dd < 10) dd = '0' + dd;
-
-  let mm = date.getMonth() + 1;
-  if (mm < 10) mm = '0' + mm;
-
+  let dd = date.getDate().toString().padStart(2, '0');
+  let mm = (date.getMonth() + 1).toString().padStart(2, '0')
   let yy = date.getFullYear()
 
-  return dd + '.' + mm + '.' + yy;
+  let hh = date.getHours().toString().padStart(2, '0')
+  let ii = date.getMinutes().toString().padStart(2, '0')
+  let ss = date.getSeconds().toString().padStart(2, '0')
+
+  return `${dd}.${mm}.${yy} ${hh}:${ii}:${ss}`;
 }
 
-const currentDate = formatDate(new Date());
+const currentFullDate = computed(() => formatDate(new Date()))
+
 </script>
 
 <template>
-  <h1>{{ currentDate}}</h1>
+  <h1>{{ currentFullDate }}</h1>
 </template>
 
 <style scoped>
