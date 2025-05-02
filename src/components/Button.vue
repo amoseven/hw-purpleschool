58ca613a42e106b1b93b0c21f328d2eabd77df10
Amoseven <omsk.web@gmail.com>
2025-05-02 23:47:17 +0600

    исправлены названия custom property

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -10,15 +10,15 @@
 
 <style scoped>
 .button {
-  font-family: var(--font);
+  font-family: var(--font-family);
   width: 335px;
   height: 68px;
   border-radius: 100px;
   padding: 16px 10px;
   line-height: 36px;
   font-size: 24px;
-  color: var(--color-primary);
-  background: var(--color-bg-main);
+  color: var(--color-card);
+  background: var(--color-primary);
   border: none;
 }
 </style>
