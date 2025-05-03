1783fb3e6ae87c41172c04d4336427a4f2624771
Amoseven <omsk.web@gmail.com>
2025-05-03 18:42:18 +0600

    карточки ответов

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
 import Button from "./components/Button.vue";
+import Header from "./components/Header.vue";
+import Cards from "./components/Cards.vue";
+
+function showTranslate() {
+  console.log("showTranslate from emit")
+}
+function changeStatus(value) {
+  console.log("changeStatus emit with value = ", value)
+}
 </script>
 
 <template>
+  <Header />
+  <Cards @show-translate="showTranslate" @change-status="changeStatus" />
   <Button>Начать игру</Button>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Score from "./Score.vue";
+import {ref} from "vue";
+
+const scores = ref(100)
+</script>
+
+<template>
+<div class="app-header">
+<p class="app-name">Запомни слово</p>
+  <Score :scores="scores" />
+</div>
+</template>
+
+<style scoped>
+.app-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1440px;
+  height: 121px;
+
+}
+.app-name {
+  color: var(--color-card-inverted);
+  text-transform: uppercase;
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+
+}
+</style>
