8ed2098cd87b54d8f0602edff9efdbb3cbfcfe29
Amoseven <omsk.web@gmail.com>
2025-05-03 19:46:00 +0600

    удалён компонент Button

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,7 +1,12 @@
 <script setup>
 import Heart from "./icons/Heart.vue";
 
-const { scores } = defineProps(['scores'])
+const { scores } = defineProps({
+  scores: {
+    type: Number,
+    default: 0
+  }
+})
 </script>
 
 <template>
